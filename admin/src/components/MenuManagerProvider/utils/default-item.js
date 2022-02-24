6267bc0a68b7e933981b8371a8a8e60afbfbcac9
Matt Milburn <me@mattmilburn.com>
2022-02-24 17:51:54 -0600

    Remove old code from pre-public development

--- admin/src/components/MenuManagerProvider/utils/default-item.js
+++ admin/src/components/MenuManagerProvider/utils/default-item.js
@@ -6,21 +6,4 @@ export default {
   target: null,
   root_menu: null,
   parent: null,
-
-  /**
-   * @TODO - Make additional fields configurable... or at least extendable.
-   * Also important to note that these cannot be empty strings.
-   */
-  title_mobile: null,
-  icon: null,
-
-  /**
-   * @TODO - Make additional relations configurable... or at least extendable.
-   */
-  page: null,
-  post: null,
-  post_category: null,
-  faq: null,
-  help: null,
-  loan: null,
 };
