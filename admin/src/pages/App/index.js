c2d00c68b7eceded15300615ecb4237aa9b9def3
Matt Milburn <me@mattmilburn.com>
2022-02-25 11:11:52 -0600

    Use QueryClientProvider in plugin

--- admin/src/pages/App/index.js
+++ admin/src/pages/App/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import { NotFound } from '@strapi/helper-plugin';
 
 import { pluginId } from '../../utils';
 import { EditView, IndexView } from '../';
 
+const queryClient = new QueryClient( {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+} );
+
 const App = () => {
   return (
-    <div>
-      <Switch>
-        <Route path={ `/plugins/${pluginId}` } component={ IndexView } exact />
-        <Route path={ `/plugins/${pluginId}/clone/:id` } component={ EditView } exact />
-        <Route path={ `/plugins/${pluginId}/edit/:id` } component={ EditView } exact />
-        <Route component={ NotFound } />
-      </Switch>
-    </div>
+    <QueryClientProvider client={ queryClient }>
+      <div>
+        <Switch>
+          <Route path={ `/plugins/${pluginId}` } component={ IndexView } exact />
+          <Route path={ `/plugins/${pluginId}/clone/:id` } component={ EditView } exact />
+          <Route path={ `/plugins/${pluginId}/edit/:id` } component={ EditView } exact />
+          <Route component={ NotFound } />
+        </Switch>
+      </div>
+    </QueryClientProvider>
   );
 };
 
