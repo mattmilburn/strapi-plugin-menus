2c96249832c8705393f43829807384eded365884
Matt Milburn <me@mattmilburn.com>
2022-03-25 14:50:25 -0500

    Minor code cleanup

--- admin/src/reducers/config.js
+++ admin/src/reducers/config.js
@@ -6,16 +6,16 @@ const initialState = {
   isLoading: true,
   config: {
     maxDepth: null,
+    layouts: {},
   },
 };
 
 const configReducer = produce( ( state = initialState, action ) => {
   switch ( action.type ) {
-    case RESOLVE_CONFIG: {
+    case RESOLVE_CONFIG:
       state.isLoading = false;
       state.config = action.data;
       break;
-    }
 
     default:
       return state;
--- server/utils/get-service.js
+++ server/utils/get-service.js
@@ -2,8 +2,6 @@
 
 const pluginId = require( './plugin-id' );
 
-const getService = name => {
-  return strapi.plugin( pluginId ).service( name );
-};
+const getService = name => strapi.plugin( pluginId ).service( name );
 
 module.exports = getService;
