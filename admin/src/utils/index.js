0487a35e4e83f2688e0542195a8f9d60b4aaf280
Matt Milburn <me@mattmilburn.com>
2022-03-31 16:53:24 -0500

    Update menus config to support simpler input format

--- admin/src/utils/index.js
+++ admin/src/utils/index.js
@@ -4,6 +4,7 @@ export { default as getBoxProps } from './get-box-props';
 export { default as getRequestUrl } from './get-request-url';
 export { default as getTrad } from './get-trad';
 export { default as menuItemProps } from './menu-item-props';
+export { default as normalizeItemFields } from './normalize-item-fields';
 export { default as pluginId } from './plugin-id';
 export { default as pluginName } from './plugin-name';
 export { default as sanitizeEntity } from './sanitize-entity';
