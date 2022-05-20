715bccd02f8544b42d603df142fa6f5a3aa74c34
Matt Milburn <me@mattmilburn.com>
2022-05-20 12:07:02 -0500

    Adjust styles for edit menu card to better fit Strapi styles

--- admin/src/components/Toolbar/styled.js
+++ admin/src/components/Toolbar/styled.js
@@ -2,8 +2,14 @@ import styled from 'styled-components';
 import { IconButtonGroup } from '@strapi/design-system/IconButton';
 
 export const StyledIconButtonGroup = styled( IconButtonGroup )`
-  span:first-child button {
-    border-left: 0;
+  span {
+    &:first-child button {
+      border-left: 0;
+    }
+
+    &:first-child:last-child button {
+      border-radius: ${({ theme }) => theme.borderRadius};
+    }
   }
 
   button {
