6cd0cd9c7c8294cc8c6ff30dc5895975784f7914
Matt Milburn <me@mattmilburn.com>
2022-07-19 13:35:20 -0500

    Create transformResponse util function

---
+++ admin/src/utils/transform-response.js
@@ -0,0 +1,39 @@
+import { has, head } from 'lodash';
+
+const transformResponse = data => {
+  if ( has( data, 'data' ) ) {
+    return transformResponse( data.data );
+  }
+
+  // Single entry.
+  if ( has( data, 'attributes' ) ) {
+    return transformResponse( {
+      id: data.id,
+      ...data.attributes,
+    } );
+  }
+
+  // Array of entries.
+  if ( Array.isArray( data ) && data.length && has( head( data ), 'attributes' ) ) {
+    return data.map( item => transformResponse( item ) );
+  }
+
+  // Loop through properties.
+  if ( has( data, 'id' ) ) {
+    Object.entries( data ).forEach( ( [ key, value ] ) => {
+      // Do nothing for null fields.
+      if ( ! value ) {
+        return;
+      }
+
+      // Relations.
+      if ( has( value, 'data' ) ) {
+        data[ key ] = transformResponse( value.data );
+      }
+    } );
+  }
+
+  return data;
+};
+
+export default transformResponse;
