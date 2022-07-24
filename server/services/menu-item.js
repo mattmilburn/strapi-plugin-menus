9913c979b59e9449ff5c4c5a061949fd49114a46
Matt Milburn <me@mattmilburn.com>
2022-07-24 14:02:46 -0500

    Refactor menu item create and update methods to handle promises better

--- server/services/menu-item.js
+++ server/services/menu-item.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { flattenDeep } = require( 'lodash' );
+const { flattenDeep, omit } = require( 'lodash' );
 const { createCoreService } = require('@strapi/strapi').factories;
 
 const { UID_MENU_ITEM } = require( '../constants' );
@@ -8,72 +8,80 @@ const { sanitizeEntity } = require( '../utils' );
 
 module.exports = createCoreService( UID_MENU_ITEM, ( { strapi } ) => ( {
   async bulkCreateOrUpdate( items, menuId ) {
-    const itemsToUpdate = items.filter( item => Number.isInteger( item.id ) );
     const itemsToCreate = items.filter( item => ! item.id || `${item.id}`.includes( 'create' ) );
+    let itemsToUpdate = items.filter( item => Number.isInteger( item.id ) );
 
-    // Maybe create root menu items before others.
-    const firstRootItemsToCreate = itemsToCreate.filter( item => ! item.parent );
+    /**
+     * @NOTE - Recursive logic CAN work to create parent items before its descendants
+     * so the parent IDs can be used in the next create operation. But managing
+     * the promise state is too complex. That is what led to assigning parent
+     * relations AFTER the items are created.
+     */
 
-    // If there are no root items to create, maybe start with items that already
-    // have an existing parent.
-    const firstChildItemsToCreate = itemsToCreate.filter( item => item.parent && typeof item.parent.id !== 'string' );
+    // Create array of promises containing create operations.
+    let createdItems = await Promise.all( itemsToCreate.map( item => {
+      // If this item has a parent being created in the same operation, we will
+      // omit that parent for now. It will be properly assigned after this.
+      const sanitizedItem = sanitizeEntity(
+        item.parent && typeof item.parent.id === 'string'
+          ? omit( item, 'parent' )
+          : item
+      );
 
-    const firstItemsToCreate = [
-      ...firstRootItemsToCreate,
-      ...firstChildItemsToCreate,
-    ];
+      return strapi.entityService.create( UID_MENU_ITEM, {
+        data: {
+          ...sanitizedItem,
+          root_menu: menuId,
+        }
+      } ).then( data => {
+        // After the data is returned, we attach `createId` to remember it's
+        // original ID so in the next operation we can assign it as the parent
+        // of other menu items which don't know the correct ID until after creation.
+        return {
+          ...data,
+          createId: item.id,
+        };
+      } );
+    } ) );
 
-    // If a parent AND child item are being created in the same save operation,
-    // we need to ensure that the parent is created before the child by returning
-    // promises that chain together additional promises to create descendants.
-    const createLoop = _items => {
-      return _items.map( async item => {
-        const sanitizedItem = sanitizeEntity( item );
-        const createdItem = await strapi.entityService.create( UID_MENU_ITEM, {
-          data: {
-            ...sanitizedItem,
-            root_menu: menuId,
-          },
-        } );
+    // For items created in this operation whose parent was also created in this
+    // same operation, we add those items into `itemsToUpdate` so they have their
+    // parent relations properly assigned.
+    itemsToCreate.forEach( item => {
+      const hasNewParent = item.parent && typeof item.parent.id === 'string';
 
-        // If this item has children that also need to be created, keep the loop going.
-        const nextItems = itemsToCreate
-          .filter( _item => _item.parent && _item.parent.id === item.id )
-          .map( _item => ( {
-            ..._item,
-            parent: {
-              id: createdItem.id,
-            }
-          } ) );
+      if ( hasNewParent ) {
+        const createdItem = createdItems.find( _item => _item.createId === item.id );
+        const createdParent = createdItems.find( _item => _item.createId === item.parent.id );
 
-        if ( nextItems.length ) {
-          const nextCreatedItems = createLoop( nextItems );
-
-          return [
-            createdItem,
-            ...nextCreatedItems,
-          ];
+        if ( ! createdItem || ! createdParent ) {
+          return;
         }
 
-        return createdItem;
-      } );
-    };
+        itemsToUpdate.push( {
+          id: createdItem.id,
+          parent: {
+            id: createdParent.id,
+          },
+        } );
+      }
+    } );
 
-    // Create arrays of promises that will handle create/update operations in order.
-    const promisedItemsToCreate = flattenDeep( createLoop( firstItemsToCreate ) );
-
-    const promisedItemsToUpdate = itemsToUpdate.map( async item => {
+    // Create array of promises containing update operations.
+    const updatedItems = await Promise.all( itemsToUpdate.map( async item => {
       const sanitizedItem = sanitizeEntity( item );
 
       return await strapi.entityService.update( UID_MENU_ITEM, item.id, {
         data: sanitizedItem,
       } );
-    } );
+    } ) );
 
-    return await Promise.all( [
-      ...promisedItemsToCreate,
-      ...promisedItemsToUpdate,
-    ] );
+    // Return created and updated items, but remove duplicates from created items
+    // because those duplicates were used to assign parent relations.
+    return [
+      ...createdItems.filter( item => ! updatedItems.find( _item => _item.id === item.id ) ),
+      ...updatedItems,
+    ];
   },
 
   async bulkDelete( items ) {
