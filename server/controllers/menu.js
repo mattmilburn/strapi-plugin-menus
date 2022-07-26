d8a3ee67906215eb40c0009a13c44eb1afb1d34a
Matt Milburn <me@mattmilburn.com>
2022-07-26 16:36:51 -0500

    Ensure nested response data can be returned by create update and find methods

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -111,7 +111,8 @@ module.exports = createCoreController( UID_MENU, ( { strapi } ) =>  ( {
   },
 
   async create( ctx ) {
-    const { data } = parseBody( ctx );
+    const { query } = ctx.request;
+    const { data, files } = parseBody( ctx );
 
     if ( ! isObject( data ) ) {
       throw new ValidationError( 'Missing "data" payload in the request body' );
@@ -125,12 +126,33 @@ module.exports = createCoreController( UID_MENU, ( { strapi } ) =>  ( {
       return ctx.badRequest( errorMessage, { slug: errorMessage } );
     }
 
-    return await super.create( ctx );
+    // Set vars related to nesting data.
+    const isNested = Object.keys( query ).includes( 'nested' );
+    const params = isNested ? getNestedParams( query ) : query;
+    const keepParentData = hasParentPopulation( query );
+
+    // Create new menu.
+    const sanitizedInputData = await this.sanitizeInput( data, ctx );
+    const entity = await getService( 'menu' ).create( {
+      ...params,
+      data: sanitizedInputData,
+      files,
+    } );
+    const sanitizedEntity = await this.sanitizeOutput( entity, ctx );
+    const transformedEntity = this.transformResponse( sanitizedEntity );
+
+    // Maybe return results in a nested format.
+    if ( isNested ) {
+      return serializeNestedMenu( transformedEntity, keepParentData );
+    }
+
+    return transformedEntity;
   },
 
   async update( ctx ) {
     const { id } = ctx.params;
-    const { data } = parseBody( ctx );
+    const { query } = ctx.request;
+    const { data, files } = parseBody( ctx );
 
     if ( ! isObject( data ) ) {
       throw new ValidationError( 'Missing "data" payload in the request body' );
@@ -144,6 +166,26 @@ module.exports = createCoreController( UID_MENU, ( { strapi } ) =>  ( {
       return ctx.badRequest( errorMessage, { slug: errorMessage } );
     }
 
-    return await super.update( ctx );
+    // Set vars related to nesting data.
+    const isNested = Object.keys( query ).includes( 'nested' );
+    const params = isNested ? getNestedParams( query ) : query;
+    const keepParentData = hasParentPopulation( query );
+
+    // Update menu.
+    const sanitizedInputData = await this.sanitizeInput( data, ctx );
+    const entity = await getService( 'menu' ).update( id, {
+      ...params,
+      data: sanitizedInputData,
+      files,
+    } );
+    const sanitizedEntity = await this.sanitizeOutput( entity, ctx );
+    const transformedEntity = this.transformResponse( sanitizedEntity );
+
+    // Maybe return results in a nested format.
+    if ( isNested ) {
+      return serializeNestedMenu( transformedEntity, keepParentData );
+    }
+
+    return transformedEntity;
   },
 } ) );
