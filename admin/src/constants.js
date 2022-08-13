34cd8b2f9118afabc26fae7866faa5dac1aad22d
Matt Milburn <me@mattmilburn.com>
2022-08-13 14:11:18 -0500

    Use fixed positioning when scrolling for EditMenuItem

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,5 +1,7 @@
 import pluginId from './utils/plugin-id';
 
+export const HEADER_HEIGHT = 96;
+
 export const RESOLVE_CONFIG = `${pluginId}/resolve-config`;
 
 export const TIME_HHMM_REGEX = new RegExp(
