c983b33dce67dabbc5318efaf6dc244cc9142b45
Thomas Brunström <choice@warzone.se>
2022-09-09 14:00:32 +0200

    Added a slash to the beginning of the endpoint so the service dont request the wrong address

--- server/services/plugin.js
+++ server/services/plugin.js
@@ -1,78 +1,92 @@
-'use strict';
+"use strict";
 
-const { get, without } = require( 'lodash' );
+const {get, without} = require("lodash");
 
-const config = require( '../config' );
-const { UID_MENU, UID_MENU_ITEM } = require( '../constants' );
-const { pluginId } = require( '../utils' );
+const config = require("../config");
+const {UID_MENU, UID_MENU_ITEM} = require("../constants");
+const {pluginId} = require("../utils");
 
-module.exports = ( { strapi } ) => ( {
+module.exports = ({strapi}) => ({
   async getConfig() {
-    const data = await strapi.config.get( `plugin.${pluginId}`, config.default );
+    const data = await strapi.config.get(`plugin.${pluginId}`, config.default);
 
     return data;
   },
 
   async getSchema() {
-    const contentTypes = strapi.plugin( 'content-manager' ).service( 'content-types' );
-    const menuModel = strapi.getModel( UID_MENU );
-    const menuItemModel = strapi.getModel( UID_MENU_ITEM );
-    const menuItemConfig = await contentTypes.findConfiguration( menuItemModel );
+    const contentTypes = strapi
+      .plugin("content-manager")
+      .service("content-types");
+    const menuModel = strapi.getModel(UID_MENU);
+    const menuItemModel = strapi.getModel(UID_MENU_ITEM);
+    const menuItemConfig = await contentTypes.findConfiguration(menuItemModel);
 
     // Determine custom relation fields, if any.
-    const editItemRelations = get( menuItemConfig, 'layouts.editRelations', [] );
-    const customItemRelations = without( editItemRelations, 'parent', 'root_menu' );
+    const editItemRelations = get(menuItemConfig, "layouts.editRelations", []);
+    const customItemRelations = without(
+      editItemRelations,
+      "parent",
+      "root_menu"
+    );
 
     // For the `MenuItem` schema, we're going to append extra metadata for custom
     // relations to more easily provide their necessary config on the frontend.
-    const menuItemAttributes = await customItemRelations.reduce( async ( prevPromise, name ) => {
-      const acc = await prevPromise;
-      const attr = acc[ name ];
+    const menuItemAttributes = await customItemRelations.reduce(
+      async (prevPromise, name) => {
+        const acc = await prevPromise;
+        const attr = acc[name];
 
-      if ( ! attr || ! attr.target ) {
-        return acc;
-      }
-
-      const relationModel = strapi.getModel( attr.target );
-
-      if ( ! relationModel ) {
-        return acc;
-      }
-
-      const relationConfig = await contentTypes.findConfiguration( relationModel );
-      const mainFieldName = get( relationConfig, 'settings.mainField' );
-      const mainFieldType = get( relationModel, `attributes.${mainFieldName}.type` );
-
-      const metadata = {
-        relationType: attr.relation,
-        targetModel: attr.target,
-        mainField: {
-          name: mainFieldName,
-          schema: {
-            type: mainFieldType,
-          },
-        },
-        queryInfos: {
-          containsKey: '',
-          defaultParams: {},
-          endPoint: `menus/relations/${name}`,
-          shouldDisplayRelationLink: true,
-          paramsToKeep: [],
-        },
-      };
-
-      return {
-        ...acc,
-        [ name ]: {
-          ...attr,
-          metadata,
+        if (!attr || !attr.target) {
+          return acc;
         }
-      };
-    }, Promise.resolve( menuItemModel.attributes ) );
+
+        const relationModel = strapi.getModel(attr.target);
+
+        if (!relationModel) {
+          return acc;
+        }
+
+        const relationConfig = await contentTypes.findConfiguration(
+          relationModel
+        );
+        const mainFieldName = get(relationConfig, "settings.mainField");
+        const mainFieldType = get(
+          relationModel,
+          `attributes.${mainFieldName}.type`
+        );
+
+        const metadata = {
+          relationType: attr.relation,
+          targetModel: attr.target,
+          mainField: {
+            name: mainFieldName,
+            schema: {
+              type: mainFieldType,
+            },
+          },
+          queryInfos: {
+            containsKey: "",
+            defaultParams: {},
+            endPoint: `/menus/relations/${name}`,
+            shouldDisplayRelationLink: true,
+            paramsToKeep: [],
+          },
+        };
+
+        return {
+          ...acc,
+          [name]: {
+            ...attr,
+            metadata,
+          },
+        };
+      },
+      Promise.resolve(menuItemModel.attributes)
+    );
 
     return {
       menu: menuModel.attributes,
       menuItem: menuItemAttributes,
     };
   },
-} );
+});
