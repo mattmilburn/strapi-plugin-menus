ce6be15effc4045af9e4bba27b8ac45a537df68f
Matt Milburn <me@mattmilburn.com>
2022-11-18 15:39:26 -0600

    Fix core component imports

--- admin/src/coreComponents/index.js
+++ admin/src/coreComponents/index.js
@@ -1,4 +1,4 @@
 // The following components were cloned from Strapi's core files with minor mods.
 export { default as InputUID } from './InputUID';
-export { default as RelationInput } from './RelationInput';
-export { default as RelationInputDataManager } from './RelationInputDataManager';
+export { RelationInput } from './RelationInput';
+export { RelationInputDataManager } from './RelationInputDataManager';
