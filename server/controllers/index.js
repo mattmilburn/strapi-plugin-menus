2fa30c17c6e6fc8d92304ee3c4e6c80bc9696f4f
Matt Milburn <me@mattmilburn.com>
2022-12-02 14:20:26 -0600

    Clone relations routes and controllers from content manager

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,8 +2,10 @@
 
 const menu = require( './menu' );
 const menuItem = require( './menu-item' );
+const relations = require( './relations' );
 
 module.exports = {
   menu,
   'menu-item': menuItem,
+  relations,
 };
--- server/routes/admin-api/index.js
+++ server/routes/admin-api/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const menu = require( './menu' );
+const relations = require( './relations' );
 
 module.exports = {
   type: 'admin',
   routes: [
+    ...relations,
     ...menu,
   ],
 };
