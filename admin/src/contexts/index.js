2c7caa33d7fe7668417d2e86baa16e20f02de02e
Matt Milburn <me@mattmilburn.com>
2023-02-02 14:22:42 -0600

    Migrate additional components to enable custom drag layer

--- admin/src/contexts/index.js
+++ admin/src/contexts/index.js
@@ -1,1 +1,2 @@
+export { default as LayoutDndContext } from './layout-dnd';
 export { default as MenuDataContext } from './menu-data';
--- admin/src/coreComponents/index.js
+++ admin/src/coreComponents/index.js
@@ -1,4 +1,6 @@
 // The following components were cloned from Strapi's core files with minor mods.
+export { default as DragLayer } from './DragLayer';
 export { default as InputUID } from './InputUID';
+export { default as LayoutDndProvider } from './LayoutDndProvider';
 export { RelationInput } from './RelationInput';
 export { RelationInputDataManager } from './RelationInputDataManager';
