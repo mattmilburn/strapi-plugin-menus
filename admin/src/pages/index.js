5c3fc7c8b3188c5590dd07b10f98f732e3dc0b21
Matt Milburn <me@mattmilburn.com>
2023-04-19 13:59:22 -0500

    Create NotFound page for router fallback

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as App } from './App';
 export { default as EditView } from './EditView';
 export { default as IndexView } from './IndexView';
+export { default as NotFound } from './NotFound';
