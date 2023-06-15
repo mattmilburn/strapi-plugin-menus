3e2a424e51ff6a5b3e95cd95fd47b4e9cd1f4a95
Matt Milburn <me@mattmilburn.com>
2023-06-15 14:46:08 -0500

    Migrate useLazyComponents hook from Strapi core

--- admin/src/hooks/index.js
+++ admin/src/hooks/index.js
@@ -1,3 +1,4 @@
+export { default as useLazyComponents } from './use-lazy-components';
 export { default as useMenuData } from './use-menu-data';
 export { default as usePluginConfig } from './use-plugin-config';
 export { default as useStickyPosition } from './use-sticky-position';
