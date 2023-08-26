102242a45170d309d94032569c7fc032c46822c2
Matt Milburn <me@mattmilburn.com>
2023-08-26 14:17:20 -0500

    Clone RelationDragPreview from Strapi core

--- admin/src/coreComponents/index.js
+++ admin/src/coreComponents/index.js
@@ -1,6 +1,7 @@
 // The following components were cloned from Strapi's core files with minor mods.
-export { default as DragLayer } from './DragLayer';
+export { DragLayer } from './DragLayer';
 export { default as InputUID } from './InputUID';
 export { default as LayoutDndProvider } from './LayoutDndProvider';
+export { RelationDragPreview } from './RelationDragPreview';
 export { RelationInput } from './RelationInput';
 export { RelationInputDataManager } from './RelationInputDataManager';
